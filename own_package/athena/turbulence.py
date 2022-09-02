f5b08a0f8121482cbf33c3f82f98979a040a761c
Hitesh Das <hitesh@ncn-29.MPA-Garching.MPG.DE>
2022-09-02 18:27:40 +0200

    Added turbulence and 2d plot style sheets

--- own_package/athena/turbulence.py
+++ own_package/athena/turbulence.py
@@ -7,8 +7,6 @@ import numpy as np
 import sys
 import os
 
-import matplotlib.pyplot as plt
-
 cwd = os.path.dirname(__file__)
 package_abs_path = cwd[:-len(cwd.split('/')[-1])]
 
@@ -23,16 +21,45 @@ from timer import timer
 from v_turb import cs_calc
 import units as un
 
+def turb_vel(hst):
+    return hst.turb_vel
+
+def mach_t (hst):
+    return hst.turb_vel/hst.cs_avg
+
+def beta(hst):
+    return hst.PB_avg/hst.Pth_avg
+
+def Ma(hst):
+    va = hst.B_abs_avg
+    return hst.turb_vel/va
+
+def beta_0(hst):
+
+    T_hot = 4e6
+    rho   = 1.0
+
+    Pth  = T_hot/(un.KELVIN*un.mu)
+    Pth *= rho
+
+    return Pth/hst.PB_avg
+
+def PB(hst):
+    return hst.PB_avg
 
 # TODO: Change to using plot_2d.py for multicolor line plots
-def plot_mach (file_list, MHD_list, ncells_list, cool_flag_list):
+def plot_time_evolution (y_func, file_list, MHD_list, ncells_list, cool_flag_list, \
+                         normalise_list = {'x_norm':[None], 'y_norm':[None]}, \
+                         ax_log= {'x_log': False, 'y_log':False, 'col_log':False}, \
+                         label_list = None, linestyle='solid'):
 
     if file_list[0][-1]=='/':
         hst_add = 'Turb.hst'
     else:
         hst_add = '/Turb.hst'
 
-    fig,ax = plt.subplots(nrows=1, ncols=1)
+    x_data_list = []
+    y_data_list = []
 
     for i_fl, fl in enumerate(file_list):
 
@@ -43,20 +70,74 @@ def plot_mach (file_list, MHD_list, ncells_list, cool_flag_list):
                           MHD_flag  = MHD_list[i_fl] , \
                           cool_flag = cool_flag_list[i_fl]  )
 
-        # ax.plot(hst.time, hst.turb_vel)
+        x_data_list.append(hst.time)
+        y_data_list.append(y_func(hst))
 
-     
-
-    return fig,ax
+    fig, ax = p2.plot_multiline(x_data_list, y_data_list, ax_log=ax_log, linestyle=linestyle, \
+                                normalise_list=normalise_list, label_list=label_list) 
+    
+    return fig, ax
 
 if __name__ == "__main__":
 
-    file_loc  = '/afs/mpa/home/hitesh/remote/cobra/athena_fork_turb_box/turb_v2/'
-    file_loc += 'Turbulence/para_scan_Rlsh5_1000_res0_256_rseed_1_M_0.5_beta_100/'
-    # file_loc += 'para_scan_comp_fshear_0.1_Rlsh0_10_res0_128_rseed_1_M_0.5_beta_100/'
+    wdir_loc  = '/afs/mpa/home/hitesh/remote/cobra/athena_fork_turb_box/turb_v2/'
 
-    # file_loc += 'Turb.out2.00501.athdf'
+    file_loc1 = wdir_loc + 'Turbulence/para_scan_Rlsh4_500_res0_256_rseed_1_M_0.5_beta_100/'
+    file_loc2 = wdir_loc + 'Turbulence/para_scan_Rlsh4_500_res0_256_rseed_1_M_0.25_beta_100/'
+    file_loc3 = wdir_loc + 'Turbulence/para_scan_Rlsh1_500_res0_256_rseed_1_M_0.9_beta_100/'
 
-    fig,ax = plot_mach([file_loc], [True], [[256,256,256]], [False])
+    # file_loc1 = wdir_loc + 'para_scan_comp_fshear_0.1_Rlsh0_10_res0_128_rseed_1_M_0.5_beta_100/'
+    # file_loc2 = wdir_loc + 'para_scan_comp_fshear_0.3_Rlsh0_10_res0_128_rseed_1_M_0.5_beta_100/'
+    # file_loc3 = wdir_loc + 'para_scan_comp_fshear_1.0_Rlsh0_10_res0_128_rseed_1_M_0.5_beta_100/'
 
+    l_shatter = 8.6e-7
+    cs = cs_calc(4e6, un.mu)
+    print(cs)
+
+    Rlsh  = 10
+    const = Rlsh*40*l_shatter/cs
+
+    # const = 0.5
+
+    arg_dict = {}
+    arg_dict['file_list']      = [file_loc1     , file_loc2    , file_loc3    ]
+    arg_dict['MHD_list' ]      = [True          , True         , True         ]
+    arg_dict['ncells_list']    = [[256,256,256] , [256,256,256], [256,256,256]]
+    # arg_dict['ncells_list']    = [[128,128,128] , [128,128,128], [128,128,128]]
+    arg_dict['cool_flag_list'] = [False         , False        , False        ]
+    arg_dict['label_list']     = [r'$\mathcal{M = 0.5}$', r'$\mathcal{M = 0.25}$', r'$\mathcal{M = 0.9}$']
+    # arg_dict['label_list']     = [r'$f_{\rm shear} = 0.1$', r'$f_{\rm shear} = 0.3$', r'$f_{\rm shear} = 1.0$']
+    arg_dict['normalise_list'] = {'x_norm':[const/0.5,const/0.25,const/0.9], 'y_norm':[None]}
+    # arg_dict['normalise_list'] = {'x_norm':[const/0.5,const/0.5,const/0.5], 'y_norm':[None]}
+
+    # fig,ax = plot_time_evolution(y_func = turb_vel , **arg_dict )
+    # plt.show()
+
+    import matplotlib.pyplot as plt
+    # plt.style.use('dark_background')
+
+    style_lib  = '../plot/style_lib/' 
+    # pallette   = style_lib + 'dark_pallette.mplstyle'
+    pallette   = style_lib + 'bright_pallette.mplstyle'
+    plot_style = style_lib + 'plot_style.mplstyle'
+
+    plt.style.use([pallette, plot_style])
+
+    fig,ax = plot_time_evolution(y_func = mach_t, **arg_dict )
+    ax.axhline(1.0, linestyle='dashed')
+    ax.set_xlabel(r't/$t_{\rm eddy}$')
+    ax.set_ylabel(r'$\mathcal{M}(t)$')
+    plt.legend()
     plt.show()
+
+    arg_dict['ax_log'] = {'x_log': False, 'y_log':True, 'col_log':False}
+
+    fig,ax = plot_time_evolution(y_func = Ma, **arg_dict , linestyle='dashed')
+    ax.set_xlabel(r't/$t_{\rm eddy}$')
+    # ax.scatter(50,0.5)
+    # ax.set_ylabel(r'$P_{\rm B}$')
+    # ax.set_ylabel(r'$\beta$')
+    ax.set_ylabel(r'$\mathcal{M}_{A}$')
+    plt.legend()
+    plt.show()
+    # plt.savefig('test.png')
--- own_package/plot/plot_2d.py
+++ own_package/plot/plot_2d.py
@@ -5,6 +5,8 @@
 
 # TODO: Add function to plot multicolor line plots
 
+from cProfile import label
+from re import I
 import numpy as np
 import cmasher as cr
 import sys
@@ -12,6 +14,7 @@ import os
 
 import matplotlib as mt
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as pe
 
 cwd = os.path.dirname(__file__)
 package_abs_path = cwd[:-len(cwd.split('/')[-1])]
@@ -21,12 +24,17 @@ from timer import timer
 import units as un
 
 
-def plot_multiline(y_data_list, x_data_list, \
+def plot_multiline(x_data_list, y_data_list, \
                    color_list=None, \
                    ax_log= {'x_log': False, 'y_log':False, 'col_log':False}, \
+                   normalise_list = {'x_norm':[None], 'y_norm':[None]}, \
+                   label_list = None, linestyle='solid', \
                    cmap=cr.rainforest):
 
-    fig, ax = plt.subplots(nrow=1, ncols=1)
+    line_border_color = mt.rcParams['lines.color']
+    line_border_width = mt.rcParams['lines.linewidth'] + 1
+
+    fig, ax = plt.subplots(nrows=1, ncols=1)
 
     L = len(y_data_list)
 
@@ -42,13 +50,43 @@ def plot_multiline(y_data_list, x_data_list, \
         cmap_fn = mt.cm.get_cmap(cmap)
         line_col = cmap_fn((cb_qnt-cb_qnt.min())/(cb_qnt.max()-cb_qnt.min()))
 
-    for i in range(L):
-        ax.plot(y_data_list[i], x_data_list[i], color=line_col[i])
 
+
+    if None in normalise_list['x_norm']:
+        print("plot_2d: None found in normalise_list['x_norm'] ...")
+        print("plot_2d: x-axis normalisation set to 1.0 ...")
+
+        normalise_list['x_norm'] = [1.0 for i in range(len(y_data_list))]
+
+    if None in normalise_list['y_norm']:
+        print("plot_2d: None found in normalise_list['y_norm'] ...")
+        print("plot_2d: y-axis normalisation set to 1.0 ...")
+
+        normalise_list['y_norm'] = [1.0 for i in range(len(y_data_list))]
+        
+
+    if label_list == None:
+        label_list = [None for i in range(len(y_data_list))]
+
+
+
+    for i in range(L):
+
+        # ax.plot(x_data_list[i]/normalise_list['x_norm'][i], \
+        #         y_data_list[i]/normalise_list['y_norm'][i], \
+        #         linestyle = linestyle, \
+        #         color=line_border_color, linewidth = line_border_width)
+
+        ax.plot(x_data_list[i]/normalise_list['x_norm'][i], \
+                y_data_list[i]/normalise_list['y_norm'][i], \
+                color=line_col[i],  linestyle=linestyle, \
+                label = label_list[i], 
+                path_effects=[pe.Stroke(linewidth=line_border_width, \
+                                        foreground=line_border_color), pe.Normal()])
 
     if ax_log['x_log']:
-        ax.xscale('log')
+        ax.set_xscale('log')
     if ax_log['y_log']:
-        ax.yscale('log')
+        ax.set_yscale('log')
 
     return fig, ax
