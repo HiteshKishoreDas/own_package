e7841b9f03574f9a3fb090dd1042e556ee4374f6
Hitesh Das <hitesh@ncn-29.MPA-Garching.MPG.DE>
2022-10-12 17:29:12 +0200

    Added projection plot

--- own_package/plot/plot_2d_image.py
+++ own_package/plot/plot_2d_image.py
@@ -81,6 +81,55 @@ def plot_slice  (img_data, slice_dir=2, x_data=None, y_data=None, \
 
     return plt_dict
 
+def plot_projection (img_data, proj_dir=2, x_data=None, y_data=None, \
+                     z_data=None, weight_data=None,\
+                     ax_log= {'x_log': False, 'y_log':False, 'col_log':False}, \
+                     normalise_list = {'x_norm':[None], 'y_norm':[None]}, \
+                     color_range = [None, None],\
+                     cmap=cr.rainforest):
+
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+    plt.tight_layout()
+
+    L = np.shape(img_data)
+    dim = len(L) 
+
+    x_dir = (proj_dir+1)%dim
+    y_dir = (proj_dir+2)%dim
+    z_dir = proj_dir
+
+    if x_data == None:
+        x_data = np.linspace(0, L[x_dir], num=L[x_dir]+1)
+    if y_data == None:
+        y_data = np.linspace(0, L[y_dir], num=L[y_dir]+1)
+    if z_data == None:
+        z_data = np.linspace(0, L[z_dir], num=L[z_dir]+1)
+
+    if weight_data == None:
+        weight_data = np.ones_like(img_data)
+
+    proj_plot = np.average(img_data, weights=weight_data, axis=proj_dir)
+
+    if proj_dir==1:
+        slc = ax.pcolormesh(x_data, y_data, proj_plot, \
+                        vmin=color_range[0], vmax=color_range[1],  \
+                        cmap=cmap  )
+    else:
+        slc = ax.pcolormesh(y_data, x_data, proj_plot, \
+                        vmin=color_range[0], vmax=color_range[1],  \
+                        cmap=cmap  )
+
+    cbar = fig.colorbar(slc, ax=ax)
+
+    ax.set_aspect('equal')
+
+    plt_dict = {}
+    plt_dict['fig']  = fig
+    plt_dict['ax']   = ax
+    plt_dict['slc']  = slc
+    plt_dict['cbar'] = cbar
+
+    return plt_dict
 
 if __name__ == "__main__":
 
@@ -98,6 +147,14 @@ if __name__ == "__main__":
     plot_dict = plot_slice(rho[:10,:20,:30], slice_dir=2)
     plot_dict['cbar'].set_label('Colorbar label') 
     plot_dict['ax'].set_title('plot_slice test') 
+    plot_dict['ax'].set_xlabel('x label') 
+    plot_dict['ax'].set_ylabel('y label') 
+
+    plt.show()
+
+    plot_dict = plot_projection(rho[:10,:20,:30], proj_dir=2)
+    plot_dict['cbar'].set_label('Colorbar label') 
+    plot_dict['ax'].set_title('plot_projection test') 
     plot_dict['ax'].set_xlabel('x label') 
     plot_dict['ax'].set_ylabel('y label') 
 
