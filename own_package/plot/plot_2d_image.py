9b12ac36d4312a512a54e034146b2720526eda97
Hitesh Kishore Das <hitesh@freya01.bc.rzg.mpg.de>
2022-11-30 17:19:37 +0100

    Routine

--- own_package/plot/plot_2d_image.py
+++ own_package/plot/plot_2d_image.py
@@ -487,7 +487,7 @@ def parallel_plot_fn (n_snap: int,  \
     if 'rho' or 'all' in field_list:
         quant_dict['rho'] = {}
         quant_dict['rho']['title'] = 'Density'
-        quant_dict['rho']['save_loc'] = f"{sim_loc}Plots/slices/rho/rho_{str(n_snap).zfill(5)}.png"
+        quant_dict['rho']['save_loc'] = f"{sim_loc}Plots/{save_dir}/rho/rho_{str(n_snap).zfill(5)}.png"
 
         quant_dict['rho']['arg_dict'] = {}
         quant_dict['rho']['arg_dict']['img_data'] = out_dict['rho']
@@ -497,7 +497,7 @@ def parallel_plot_fn (n_snap: int,  \
     if 'log_rho' or 'all' in field_list:
         quant_dict['rho'] = {}
         quant_dict['rho']['title'] = 'Log_10 Density'
-        quant_dict['rho']['save_loc'] = f"{sim_loc}Plots/slices/rho/rho_{str(n_snap).zfill(5)}.png"
+        quant_dict['rho']['save_loc'] = f"{sim_loc}Plots/{save_dir}/log_rho/log_rho_{str(n_snap).zfill(5)}.png"
 
         quant_dict['rho']['arg_dict'] = {}
         quant_dict['rho']['arg_dict']['img_data'] = np.log10(out_dict['rho'])
@@ -508,7 +508,7 @@ def parallel_plot_fn (n_snap: int,  \
     if 'prs' or 'all' in field_list:
         quant_dict['prs'] = {}
         quant_dict['prs']['title'] = 'Pressure'
-        quant_dict['prs']['save_loc'] = f"{sim_loc}Plots/slices/prs/prs_{str(n_snap).zfill(5)}.png"
+        quant_dict['prs']['save_loc'] = f"{sim_loc}Plots/{save_dir}/prs/prs_{str(n_snap).zfill(5)}.png"
 
         quant_dict['prs']['arg_dict'] = {}
         quant_dict['prs']['arg_dict']['img_data'] = np.log10(out_dict['rho'])
@@ -518,7 +518,7 @@ def parallel_plot_fn (n_snap: int,  \
     if 'logT' or 'all' in field_list:
         quant_dict['logT'] = {}
         quant_dict['logT']['title'] = 'log_10 T'
-        quant_dict['logT']['save_loc'] = f"{sim_loc}Plots/slices/logT/logT_{str(n_snap).zfill(5)}.png"
+        quant_dict['logT']['save_loc'] = f"{sim_loc}Plots/{save_dir}/logT/logT_{str(n_snap).zfill(5)}.png"
 
         quant_dict['logT']['arg_dict'] = {}
         quant_dict['logT']['arg_dict']['img_data'] = out_dict['logT']
@@ -528,7 +528,7 @@ def parallel_plot_fn (n_snap: int,  \
     if 'vx' or 'all' in field_list:
         quant_dict['vx'] = {}
         quant_dict['vx']['title'] = 'v_x'
-        quant_dict['vx']['save_loc'] = f"{sim_loc}Plots/slices/vx/vx_{str(n_snap).zfill(5)}.png"
+        quant_dict['vx']['save_loc'] = f"{sim_loc}Plots/{save_dir}/vx/vx_{str(n_snap).zfill(5)}.png"
 
         quant_dict['vx']['arg_dict'] = {}
         quant_dict['vx']['arg_dict']['img_data'] = out_dict['vel'][0]
@@ -538,7 +538,7 @@ def parallel_plot_fn (n_snap: int,  \
     if 'vy' or 'all' in field_list:
         quant_dict['vy'] = {}
         quant_dict['vy']['title'] = 'v_y'
-        quant_dict['vy']['save_loc'] = f"{sim_loc}Plots/slices/vy/vy_{str(n_snap).zfill(5)}.png"
+        quant_dict['vy']['save_loc'] = f"{sim_loc}Plots/slices/{save_dir}/vy_{str(n_snap).zfill(5)}.png"
 
         quant_dict['vy']['arg_dict'] = {}
         quant_dict['vy']['arg_dict']['img_data'] = out_dict['vel'][1]
@@ -548,7 +548,7 @@ def parallel_plot_fn (n_snap: int,  \
     if 'vz' or 'all' in field_list:
         quant_dict['vz'] = {}
         quant_dict['vz']['title'] = 'v_z'
-        quant_dict['vz']['save_loc'] = f"{sim_loc}Plots/slices/vz/vz_{str(n_snap).zfill(5)}.png"
+        quant_dict['vz']['save_loc'] = f"{sim_loc}Plots/slices/{save_dir}/vz_{str(n_snap).zfill(5)}.png"
 
         quant_dict['vz']['arg_dict'] = {}
         quant_dict['vz']['arg_dict']['img_data'] = out_dict['vel'][2]
@@ -558,7 +558,7 @@ def parallel_plot_fn (n_snap: int,  \
     if MHD_flag and ('Bx' or 'all' in field_list):
         quant_dict['Bx'] = {}
         quant_dict['Bx']['title'] = 'B_x'
-        quant_dict['Bx']['save_loc'] = f"{sim_loc}Plots/slices/Bx/Bx_{str(n_snap).zfill(5)}.png"
+        quant_dict['Bx']['save_loc'] = f"{sim_loc}Plots/slices/{save_dir}/Bx_{str(n_snap).zfill(5)}.png"
 
         quant_dict['Bx']['arg_dict'] = {}
         quant_dict['Bx']['arg_dict']['img_data'] = out_dict['B'][0]
@@ -568,7 +568,7 @@ def parallel_plot_fn (n_snap: int,  \
     if MHD_flag and ('By' or 'all' in field_list):
         quant_dict['By'] = {}
         quant_dict['By']['title'] = 'B_y'
-        quant_dict['By']['save_loc'] = f"{sim_loc}Plots/slices/By/By_{str(n_snap).zfill(5)}.png"
+        quant_dict['By']['save_loc'] = f"{sim_loc}Plots/slices/{save_dir}/By_{str(n_snap).zfill(5)}.png"
 
         quant_dict['By']['arg_dict'] = {}
         quant_dict['By']['arg_dict']['img_data'] = out_dict['B'][1]
@@ -578,7 +578,7 @@ def parallel_plot_fn (n_snap: int,  \
     if MHD_flag and ('Bz' or 'all' in field_list):
         quant_dict['Bz'] = {}
         quant_dict['Bz']['title'] = 'B_z'
-        quant_dict['Bz']['save_loc'] = f"{sim_loc}Plots/slices/Bz/Bz_{str(n_snap).zfill(5)}.png"
+        quant_dict['Bz']['save_loc'] = f"{sim_loc}Plots/slices/{save_dir}/Bz_{str(n_snap).zfill(5)}.png"
 
         quant_dict['Bz']['arg_dict'] = {}
         quant_dict['Bz']['arg_dict']['img_data'] = out_dict['B'][2]
